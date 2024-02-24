629c1dc99eb061e475940a60074cce9d70586b95
fallen egoist <gusanu00@gmail.com>
2024-02-24 14:42:38 +0200

    fonts+dynamic text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,49 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   template: `
     <div class="interface">
-      <p #storyText class="storyText">textbox</p>
+      <p id="storyText" #storyText class="storyText">gf;kdjngdfkjgfbkjdgbdjdb</p>
       
     </div>
     <p class="options">option1</p>
-    <p class="options">option2</p>
-    <p class="options">option3</p>
+    <p class="options">Decizi după acest noroc să termini cu toate infracțiunile și să trăiești o viață liniștită</p>
+    <p class="options">{{text}}</p>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  @ViewChild('storyText', { static: true }) storyTextRef!: ElementRef;
+
+ngOnInit(){
+  this.temp()
+}
+
+  // @ViewChild('storyText', { static: true }) storyTextRef!: ElementRef;
 
 text = 'merge bine'
 
-  test() {
-    if (this.storyTextRef) {
-      this.storyTextRef.nativeElement.innerText = this.text;
+  // test() {
+  //   if (this.storyTextRef) {
+  //     this.storyTextRef.nativeElement.innerText = this.text;
+  //   }
+  // }
+
+  g = document.getElementById("storyText");
+
+  temp() {
+    let g = document.getElementById("storyText");
+  
+    // Check if g is not null before accessing its properties
+    if (g !== null) {
+      g.innerText = this.text;
+      console.log('done');
+      
+    } else {
+      console.error("Element with id 'storyText' not found.");
+      // Handle the case where the element is not found, e.g., show an error message.
     }
   }
+
 }
