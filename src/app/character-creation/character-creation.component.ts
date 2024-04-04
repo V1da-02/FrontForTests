ec2fa07992c71dd7aeec9ac438ff5608c5b79333
commented <gusanu02@gmail.com>
2024-04-04 14:50:52 +0300

    character creation page improvement

--- src/app/character-creation/character-creation.component.ts
+++ src/app/character-creation/character-creation.component.ts
@@ -8,9 +8,54 @@ import { Router } from '@angular/router';
   template: `
   <div class="form">
   <form>
-    <label for="chName">Warrior name:</label>
-    <input name="chName" type="text">
+
+  <div class="nameBox">
+    <br>
+    <label for="chName">Character name: </label>
+    <br>
+    <input name="chName" type="text" placeholder="" id="chName" style="width:100%">
+  </div>
+
+    <br>
+
+
+    <div style="display:flex; align-items: center;">
+
+
+    <div class="class-card">
+    <label for="fighter"><p>Fighter</p></label>
+    <label for="fighter"><img  height="340" src="../assets/fighter.png"></label>
+    <input type="radio" id="fighter" name="class" value="fighter">
+    </div>
+
+
+    <div class="class-card">
+    <label for="paladin"><p>Paladin</p></label>
+    <label for="paladin"><img  height="340" src="../assets/paladin1.png"></label>
+    <input type="radio" id="paladin" name="class" value="paladin">
+    </div>
+
+  
+
+    
+    <div class="class-card">
+    <label for="wizard"><p>Wizard</p></label>
+    <label for="wizard"><img  height="340" src="../assets/wizard1.png"></label>
+    <input type="radio" id="wizard" name="class" value="wizard">
+    </div>
+
+    <br>
+
+    </div>
+    <br>
+    <br>
+    <br>
+    <div style="display: flex; justify-content: center; align-items: center;">
+    <input type="submit" value="Create character" class="createChBtn">
+    </div>
   </form>
+
+
 
   </div>  
   `,
